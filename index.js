25167a49574db6f00f2f7ab1e50efe23299ec1d5
Sawer <danilch4024@gmail.com>
2025-04-22 16:16:33 +0500

    2

---
+++ index.js
@@ -0,0 +1,30 @@
+document.getElementById('order-submit-button').addEventListener('click', e => {
+    e.preventDefault();
+    updateOrder();
+    document.getElementById('menu-overlay').style.display = 'flex';
+});
+
+document.getElementById('menu-close-btn').addEventListener('click', () => {
+    document.getElementById('menu-overlay').style.display = 'none';
+});
+
+function updateOrder() {
+    const count = 121;
+    document.getElementById('menu-content').textContent = `Вы заказали ${count} ${formatNumeric(count, 'напиток', 'напитка', 'напитков')}`;
+}
+
+function formatNumeric(number, form1, form2, form5) {
+    const number100 = number % 100;
+    if (number100 > 4 && number100 < 21) {
+      return form5;
+    }
+    const number10 = number % 10;
+    if (number10 === 1) {
+      return form1;
+    }
+    if (number10 > 1 && number10 < 5) {
+      return form2;
+    }
+    return form5;
+  }
+  
